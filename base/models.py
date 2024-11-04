ab13bb4f6ef9be592b891fe96c5a952485256ed6
prathamesh9125 <pdkole123@gmail.com>
2024-11-04 21:25:27 +0530

    new product model 2

--- base/models.py
+++ base/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class BaseModel(models.model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_add=True)
+    
+    class Meta:
+        abstract = True
--- products/models.py
+++ products/models.py
@@ -5,12 +5,13 @@ from base.models import BaseModel
 class Category(BaseModel):
     category_name = models.CharFile(max_lenght=100)
     category_image = models.ImageField(upload="categories")
-
+    slug = models.SlugField(unique=True, null=True, blank= True)
+    
 class Product(BaseModel):
     Product_name = models.CharFile(max_lenght=100)
     category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name="prodcuts")
     price = models.IntegerField()
-    
+    Product_discription = models.TextField()
     
 class ProductImage(BaseModel):
     Product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name="product_images")
