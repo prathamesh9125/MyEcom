5ace3048c597dc92b4d3f0a1c896d06bfd0f8e62
prathamesh9125 <pdkole123@gmail.com>
2024-11-04 22:38:37 +0530

    modified runserver 1

--- MyEcom/urls.py
+++ MyEcom/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+from django.urls import path, include
+from products.views import start_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
+    path('start_page/',start_page, name="start")
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def start_page(request):
+    return render(request,'products/start.html')
