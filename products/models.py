7e8fe8c338b8ed4c50dea038828698892295a51d
prathamesh9125 <pdkole123@gmail.com>
2024-11-04 21:46:18 +0530

    new added product migration

--- products/models.py
+++ products/models.py
@@ -3,19 +3,19 @@ from base.models import BaseModel
 
 # Create your models here.
 class Category(BaseModel):
-    category_name = models.CharFile(max_lenght=100)
-    category_image = models.ImageField(upload="categories")
+    category_name = models.CharField(max_length=100)
+    category_image = models.ImageField(upload_to="categories")
     slug = models.SlugField(unique=True, null=True, blank= True)
     
 class Product(BaseModel):
-    Product_name = models.CharFile(max_lenght=100)
+    Product_name = models.CharField(max_length=100)
     category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name="prodcuts")
     price = models.IntegerField()
     Product_discription = models.TextField()
     
 class ProductImage(BaseModel):
     Product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name="product_images")
-    image = models.ImageField(uploads="product")
+    image = models.ImageField(upload_to="product")
     
 
 
