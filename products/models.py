60039e7f3ebd0fb13894d0f706df445f60657cb1
prathamesh9125 <pdkole123@gmail.com>
2024-11-04 21:15:56 +0530

    new product model 1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from base.models import BaseModel
 
 # Create your models here.
+class Category(BaseModel):
+    category_name = models.CharFile(max_lenght=100)
+    category_image = models.ImageField(upload="categories")
+
+class Product(BaseModel):
+    Product_name = models.CharFile(max_lenght=100)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name="prodcuts")
+    price = models.IntegerField()
+    
+    
+class ProductImage(BaseModel):
+    Product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name="product_images")
+    image = models.ImageField(uploads="product")
+    
+
+
